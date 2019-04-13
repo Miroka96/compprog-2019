3a5236c300e58ef0323b0db42b72b23f2fc421cc
mirko.krause <mirko.krause@student.hpi.de>
2019-04-13 14:18:32 +0200

    improved look of IO

--- template/solution.c
+++ template/solution.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define BASE 10
 #define OUTPUT_LENGTH 10
 #define BUFFER_SIZE 4096
 
-int main(int argc, char* argv[])
-{
-    // buffer stdin
-    char inputbuffer[BUFFER_SIZE];
-    setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
+// buffer stdin
+char inputbuffer[BUFFER_SIZE];
 
-    // create output buffer
-    char outputbuffer[OUTPUT_LENGTH];
+#define read(var, c, delimeter) while (true) { \
+    if (c == delimeter) break; \
+    var = var * 10 + (c - '0'); \
+    c = fgetc(stdin);\
+}
+
+// create output buffer
+char outputbuffer[OUTPUT_LENGTH];
+
+#define write(var) if (var == 0) { \
+    putc('0', stdout); \
+    }else{ \
+    char pos = OUTPUT_LENGTH; \
+    while (var != 0) { \
+        char rem = var % BASE; \
+        var = var/BASE; \
+        outputbuffer[--pos] = rem + '0'; \
+    } \
+    for (char p = pos; p < OUTPUT_LENGTH; p++ ) { \
+        putc(outputbuffer[p], stdout); \
+    } \
+}   \
+putc('\n', stdout);
+
+
+
+int main(int argc, char* argv[]) {
+    setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
 
     while (true) {
         int c = fgetc(stdin);
@@ -21,51 +45,17 @@ int main(int argc, char* argv[])
         int n = 0;
         int m = 0;
 
-        while (true) {
-            // read first number
-            if (c == ' ') break;    // finished reading first number
-            n = n*10 + ( c - '0');
-            c = fgetc(stdin);
+        read(n, c, ' ');    // read first number
+        c = fgetc(stdin);
+        read(m, c, '\n');    // read second number
+
+        // calculate result
+        long operations = n * (long) m - 1;
+        if (operations < 0) {
+            operations = 0;
         }
 
-        while (true) {
-            // read last number
-            c = fgetc(stdin);
-
-            if (c == '\n') {        // finished reading last number
-
-
-                // calculate result
-                long operations = n * (long) m - 1;
-                if (operations < 0) {
-                    operations = 0;
-                }
-
-
-
-
-                // output result
-                if (operations == 0) {
-                    putc('0', stdout);
-                } else {
-                    char pos = OUTPUT_LENGTH;
-                    while (operations != 0) {
-                        char rem = operations % 10;
-                        operations = operations/10;
-                        outputbuffer[--pos] = rem + '0';
-                    }
-                    for (char p = pos; p < OUTPUT_LENGTH; p++ ) {
-                        putc(outputbuffer[p], stdout);
-                    }
-                }
-
-                // end line output
-                putc('\n', stdout);
-                break;
-            }
-
-            m = m*10 + (c -'0');
-        }
+        write(operations);
     }
 
     return 0;
