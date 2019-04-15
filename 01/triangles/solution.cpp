db1a40f71a89a50172e60d6c4e20716914e7ab59
mirko.krause <mirko.krause@student.hpi.de>
2019-04-15 19:04:52 +0200

    triangles working locally

--- 01/triangles/solution.cpp
+++ 01/triangles/solution.cpp
@@ -8,7 +8,7 @@
 //#define debug(x)    clog << #x << " = " << x << endl
 
 // print a debug string
-#define debugs(str) //clog << str << endl
+#define debugs(str) clog << str << endl
 #define rep(a, b)   for(int a = 0; a < (b); ++a)
 #define repd(a, b)   for(int a = 0; a < (b); debug(++a))
 #define all(a)      (a).begin(),(a).end()
@@ -28,16 +28,11 @@ bool negative_input;
 // read a number from stdin into var
 #define readn(var)  var = 0;                                \
                     c = getchar_unlocked();                 \
-                    if (c == '-') {                         \
-                        negative_input = true;              \
-                        c = getchar_unlocked();             \
-                    } else negative_input = false;          \
                     while (true) {                          \
                         if (c < '0') break;                 \
                         var = var * 10 + (c - '0');         \
                         c = getchar_unlocked();             \
                     }                                       \
-                    if (negative_input) var = -var;         \
                     debug(var);
 
 // create output buffer
@@ -61,10 +56,62 @@ putc('\n', stdout);
 
 ////////////////////////////// Task ////////////////////////
 
+//                      k =  2 3 4 5  6  7  8  9  10 11 12 13 14 15  16  17  18  19
+constexpr unsigned char TwoOutOf[] = {1,3,6,10,15,21,28,36,45,55,66,78,92,107,123,140,158,177};
+
 int main(int argc, char* argv[]) {
     setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
 
-    
+    short triplesc;
+    readn(triplesc);
+
+    unordered_map<unsigned long, short> numberCounts;
+    numberCounts.reserve(triplesc);
+
+    rep(in, triplesc){
+        unsigned long input;
+        readn(input);
+
+        short &stored_val = numberCounts[input];
+        stored_val++;
+    }
+
+    unsigned long resultCount = 0;
+
+    for (auto ifirst = numberCounts.begin(); ifirst != numberCounts.end(); ifirst++) {
+        auto firstc = ifirst->second;
+        debug(firstc);
+        auto first = ifirst->first;
+        debug(first);
+
+        if (firstc >= 2) {
+            auto third = numberCounts.find(first * 2);
+            if (third != numberCounts.end()) {
+                int twoOutOf;
+                if (firstc <= 19) {
+                    twoOutOf = TwoOutOf[firstc - 2];
+                } else {
+                    twoOutOf = firstc * (firstc - 1) / 2;
+                }
+                resultCount += twoOutOf * third->second;
+            }
+            debug(firstc);
+        }
+
+        for (auto isecond = ifirst; ++isecond != numberCounts.end();) {
+            auto secondc = isecond->second;
+            debug(secondc);
+            unsigned long sum = first + isecond->first;
+            debug(sum);
+            auto third = numberCounts.find(sum);
+            if (third != numberCounts.end()) {
+                resultCount += firstc * secondc * third->second;
+                debug(resultCount);
+            }
+        }
+    }
+
+    write(resultCount);
 
     return 0;
 }
