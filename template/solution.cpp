164372d1ddb9c252722c7f2cdedfc0fa1d555d68
mirko.krause <mirko.krause@student.hpi.de>
2019-04-15 17:06:42 +0200

    improved template

--- template/solution.cpp
+++ template/solution.cpp
@@ -1,21 +1,49 @@
 #include <bits/stdc++.h>
 
+////////////////////////////// Helper //////////////////////
+
+// disable debugging for submission
+#define debug(x)    x
+// print value of debug variable
+//#define debug(x)    clog << #x << " = " << x << endl
+
+// print a debug string
+#define debugs(str) //clog << str << endl
+#define rep(a, b)   for(int a = 0; a < (b); ++a)
+#define repd(a, b)   for(int a = 0; a < (b); debug(++a))
+#define all(a)      (a).begin(),(a).end()
+
+using namespace std;
+
+////////////////////////////// I/O /////////////////////////
 #define BASE 10
 #define OUTPUT_LENGTH 10
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 1024
 
 // buffer stdin
 char inputbuffer[BUFFER_SIZE];
 
-#define read(var, c, delimeter) while (true) { \
-    if (c == delimeter) break; \
-    var = var * 10 + (c - '0'); \
-    c = fgetc(stdin);\
-}
+int c;
+bool negative_input;
+// read a number from stdin into var
+#define readn(var)  var = 0;                                \
+                    c = getchar_unlocked();                 \
+                    if (c == '-') {                         \
+                        negative_input = true;              \
+                        c = getchar_unlocked();             \
+                    } else negative_input = false;          \
+                    while (true) {                          \
+                        if (c < '0') break;                 \
+                        var = var * 10 + (c - '0');         \
+                        c = getchar_unlocked();             \
+                    }                                       \
+                    if (negative_input) var = -var;         \
+                    debug(var);
 
 // create output buffer
 char outputbuffer[OUTPUT_LENGTH];
 
+// write var as number to stdout
 #define write(var) if (var == 0) { \
     putc('0', stdout); \
     }else{ \
@@ -31,31 +59,12 @@ char outputbuffer[OUTPUT_LENGTH];
 }   \
 putc('\n', stdout);
 
-
+////////////////////////////// Task ////////////////////////
 
 int main(int argc, char* argv[]) {
     setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
 
-    while (true) {
-        int c = fgetc(stdin);
-        if (c == EOF) break;
-
-        // initialize iteration variables
-        int n = 0;
-        int m = 0;
-
-        read(n, c, ' ');    // read first number
-        c = fgetc(stdin);
-        read(m, c, '\n');    // read second number
-
-        // calculate result
-        long operations = n * (long) m - 1;
-        if (operations < 0) {
-            operations = 0;
-        }
-
-        write(operations);
-    }
+    
 
     return 0;
 }
