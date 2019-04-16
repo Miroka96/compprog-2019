39c4fca966d08a07e6cb107e1df38a5cb7058582
mirko.krause <mirko.krause@student.hpi.de>
2019-04-16 16:33:14 +0200

    refactored triangles and added output files

--- 01/triangles/solution.cpp
+++ 01/triangles/solution.cpp
@@ -13,51 +13,61 @@
 #define repd(a, b)   for(int a = 0; a < (b); debug(++a))
 #define all(a)      (a).begin(),(a).end()
 
+#define uint unsigned int
+#define ulong unsigned long
+
 using namespace std;
 
 ////////////////////////////// I/O /////////////////////////
 #define BASE 10
-#define OUTPUT_LENGTH 10
+#define OUTPUT_LENGTH 24
 #define BUFFER_SIZE 1024
 
 // buffer stdin
 char inputbuffer[BUFFER_SIZE];
 
-int c;
-bool negative_input;
-// read a number from stdin into var
-#define readn(var)  var = 0;                                \
-                    c = getchar_unlocked();                 \
-                    while (true) {                          \
-                        if (c < '0') break;                 \
-                        var = var * 10 + (c - '0');         \
-                        c = getchar_unlocked();             \
-                    }                                       \
-                    debug(var);
+template<typename T> inline void readn(T &x) 
+{ 
+    x = 0; 
+    bool neg = 0; 
+    register T c = getchar_unlocked(); 
+  
+    if (c == '-') 
+        neg = 1, c = getchar_unlocked(); 
+  
+    for ( ; c >= '0' && c <= '9'; c = getchar_unlocked() ) 
+        x = (x << 3) + ( x << 1 ) + ( c & 15 ); 
+  
+    if (neg) x *= -1; 
+} 
 
 // create output buffer
 char outputbuffer[OUTPUT_LENGTH];
 
-// write var as number to stdout
-#define write(var) if (var == 0) { \
-    putc('0', stdout); \
-    }else{ \
-    char pos = OUTPUT_LENGTH; \
-    while (var != 0) { \
-        char rem = var % BASE; \
-        var = var/BASE; \
-        outputbuffer[--pos] = rem + '0'; \
-    } \
-    for (char p = pos; p < OUTPUT_LENGTH; p++ ) { \
-        putc(outputbuffer[p], stdout); \
-    } \
-}   \
-putc('\n', stdout);
+template<typename T> inline void write(T n) 
+{ 
+    bool neg = 0; 
+    if (n < 0) n *= -1, neg = 1; 
+  
+    int i = 0; 
+    do { 
+        outputbuffer[i++] = n % 10 + '0'; 
+        n /= 10; 
+    } while (n); 
+    --i; 
+  
+    if (neg) putchar_unlocked('-'); 
+  
+    while (i >= 0) putchar_unlocked(outputbuffer[i--]); 
+  
+    putchar_unlocked('\n'); 
+} 
 
 ////////////////////////////// Task ////////////////////////
 
-//                      k =  2 3 4 5  6  7  8  9  10 11 12 13 14 15  16  17  18  19
-constexpr unsigned char TwoOutOf[] = {1,3,6,10,15,21,28,36,45,55,66,78,92,107,123,140,158,177};
+//#define MAX_VALUE 1000000008
+
+//bitset<MAX_VALUE> bloomfilter;
 
 int main(int argc, char* argv[]) {
     setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
@@ -65,18 +75,18 @@ int main(int argc, char* argv[]) {
     short triplesc;
     readn(triplesc);
 
-    unordered_map<unsigned long, short> numberCounts;
+    unordered_map<ulong, short> numberCounts;
     numberCounts.reserve(triplesc);
 
     rep(in, triplesc){
-        unsigned long input;
+        ulong input;
         readn(input);
 
         short &stored_val = numberCounts[input];
         stored_val++;
     }
 
-    unsigned long resultCount = 0;
+    ulong resultCount = 0;
 
     for (auto ifirst = numberCounts.begin(); ifirst != numberCounts.end(); ifirst++) {
         auto firstc = ifirst->second;
@@ -87,13 +97,7 @@ int main(int argc, char* argv[]) {
         if (firstc >= 2) {
             auto third = numberCounts.find(first * 2);
             if (third != numberCounts.end()) {
-                int twoOutOf;
-                if (firstc <= 19) {
-                    twoOutOf = TwoOutOf[firstc - 2];
-                } else {
-                    twoOutOf = firstc * (firstc - 1) / 2;
-                }
-                resultCount += twoOutOf * third->second;
+                resultCount += firstc * ((ulong) firstc - 1) / 2 * third->second;
             }
             debug(firstc);
         }
@@ -101,13 +105,13 @@ int main(int argc, char* argv[]) {
         for (auto isecond = ifirst; ++isecond != numberCounts.end();) {
             auto secondc = isecond->second;
             debug(secondc);
-            unsigned long sum = first + isecond->first;
+            ulong sum = first + isecond->first;
             debug(sum);
-            auto third = numberCounts.find(sum);
-            if (third != numberCounts.end()) {
-                resultCount += firstc * secondc * third->second;
-                debug(resultCount);
-            }
+                auto third = numberCounts.find(sum);
+                if (third != numberCounts.end()) {
+                    resultCount += firstc * secondc * third->second;
+                    debug(resultCount);
+                }
         }
     }
 
