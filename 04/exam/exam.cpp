1e8f594da185ebcd1627505def97cda71eb39999
mirko.krause <mirko.krause@student.hpi.de>
2019-05-05 20:30:02 +0200

    solved exam and added test files

--- 04/exam/exam.cpp
+++ 04/exam/exam.cpp
@@ -18,43 +18,29 @@ using namespace std;
 ////////////////////////////// I/O /////////////////////////
 #define BASE 10
 #define OUTPUT_LENGTH 24
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 0x10000
 
 // buffer stdin
 char inputbuffer[BUFFER_SIZE];
 
 template <typename T> inline void readn(T &x) {
   x = 0;
-  bool neg = 0;
   register T c = getchar_unlocked();
-
-  if (c == '-')
-    neg = 1, c = getchar_unlocked();
 
   for (; c >= '0' && c <= '9'; c = getchar_unlocked())
     x = (x << 3) + (x << 1) + (c & 15);
-
-  if (neg)
-    x *= -1;
 }
 
 // create output buffer
 char outputbuffer[OUTPUT_LENGTH];
 
 template <typename T> inline void write(T n) {
-  bool neg = 0;
-  if (n < 0)
-    n *= -1, neg = 1;
-
   int i = 0;
   do {
     outputbuffer[i++] = n % 10 + '0';
     n /= 10;
   } while (n);
   --i;
-
-  if (neg)
-    putchar_unlocked('-');
 
   while (i >= 0)
     putchar_unlocked(outputbuffer[i--]);
@@ -70,8 +56,49 @@ inline void print(const char *str) {
 
 ////////////////////////////// Task ////////////////////////
 
+inline int& max(int& a, int& b) {
+  if (a > b) return a;
+  return b;
+}
+
+inline int& max(int& a, int& b, int& c) {
+  return max(a, max(b, c));
+}
+
 int main(int argc, char *argv[]) {
   setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
+
+  short w, h;
+  readn(w);
+  readn(h);
+
+  int chocolate[h][w];
+
+  rep(i, h) {
+    rep(j, w) {
+      int value;
+      readn(value);
+      chocolate[i][j] = value;
+    }
+  }
+
+  for(short i = h - 2; i >= 0; i--) {
+    chocolate[i][0] += max(chocolate[i+1][0], chocolate[i+1][1]);
+
+    for (short j = 1; j < w - 1; j++) {
+      chocolate[i][j] += max(chocolate[i + 1][j - 1], chocolate[i + 1][j], chocolate[i + 1][j + 1]);
+    }
+
+    chocolate[i][w-1] += max(chocolate[i+1][w-1], chocolate[i+1][w-2]);
+  }
+
+  int max = 0;
+  rep(j, w) {
+    if (chocolate[0][j] > max) {
+      max = chocolate[0][j];
+    }
+  }
+  write(max);
 
   return 0;
 }
