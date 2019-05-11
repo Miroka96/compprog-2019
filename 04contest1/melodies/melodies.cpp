c046a88c48783eca82eb023d6465a25053935aa3
mirko.krause <mirko.krause@student.hpi.de>
2019-05-11 12:06:03 +0200

    prepared files of latest 04contest1

---
+++ 04contest1/melodies/melodies.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+////////////////////////////// Helper //////////////////////
+
+// disable debugging for submission
+//#define debug(x)    x
+// print value of debug variable
+#define debug(x) clog << #x << " = " << x << endl
+
+// print a debug string
+#define debugs(str) // clog << str << endl
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define repd(a, b) for (int a = 0; a < (b); debug(++a))
+#define all(a) (a).begin(), (a).end()
+
+using namespace std;
+
+////////////////////////////// I/O /////////////////////////
+#define BASE 10
+#define OUTPUT_LENGTH 24
+#define BUFFER_SIZE 0x10000
+
+// buffer stdin
+char inputbuffer[BUFFER_SIZE];
+
+template <typename T> inline void readn(T &x) {
+  x = 0;
+  bool neg = 0;
+  register T c = getchar_unlocked();
+
+  if (c == '-')
+    neg = 1, c = getchar_unlocked();
+
+  for (; c >= '0' && c <= '9'; c = getchar_unlocked())
+    x = (x << 3) + (x << 1) + (c & 15);
+
+  if (neg)
+    x *= -1;
+}
+
+// create output buffer
+char outputbuffer[OUTPUT_LENGTH];
+
+template <typename T> inline void write(T n) {
+  bool neg = 0;
+  if (n < 0)
+    n *= -1, neg = 1;
+
+  int i = 0;
+  do {
+    outputbuffer[i++] = n % 10 + '0';
+    n /= 10;
+  } while (n);
+  --i;
+
+  if (neg)
+    putchar_unlocked('-');
+
+  while (i >= 0)
+    putchar_unlocked(outputbuffer[i--]);
+
+  putchar_unlocked('\n');
+}
+
+inline void print(const char *str) {
+  for (const char *c = str; *c; c++) {
+    putchar_unlocked(*c);
+  }
+}
+
+////////////////////////////// Task ////////////////////////
+
+int main(int argc, char *argv[]) {
+  setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
+
+  return 0;
+}
