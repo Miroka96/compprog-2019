b17a2fdb63ba2b8b051359a73386bd6a4406eb56
mirko.krause <mirko.krause@student.hpi.de>
2019-05-21 21:26:41 +0200

    began automaton

--- 06/automaton/automaton.cpp
+++ 06/automaton/automaton.cpp
@@ -70,8 +70,70 @@ inline void print(const char *str) {
 
 ////////////////////////////// Task ////////////////////////
 
+inline char pattern_to_n(const bool& lu, const bool& ru, const bool& lb, const bool& rb) {
+  char res = 0;
+  res += lu;
+  res += ru<<1;
+  res += lb<<2;
+  res += rb<<3;
+  return res;
+}
+
+inline bool read_field() {
+  return getchar_unlocked() == '#';
+}
+
+inline char read_2x2_pattern() {
+  bool lu, ru, lb, rb;
+  lu = read_field();
+  ru = read_field();
+  getchar_unlocked();
+  lb = read_field();
+  rb = read_field();
+  getchar_unlocked();
+  return pattern_to_n(lu, ru, lb, rb);
+}
+
+inline void read_4x4_pattern(char res[4]) {
+  bool field[4][4];
+  rep(i, 4) {
+    rep(j, 4) {
+      field[i][j] = read_field();
+    }
+    getchar_unlocked();
+  }
+
+  int k = 0;
+  for (int i = 0; i < 4; i+=2) {
+    for (int j = 0; j < 4; j+=2) {
+      res[k++] = pattern_to_n(
+        field[i][j],
+        field[i][j + 1],
+        field[i + 1][j],
+        field[i + 1][j + 1]
+      );
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
+
+  uint64_t iteration_count;
+  readn(iteration_count);
+
+  char initial = read_2x2_pattern();
+
+  char evolutions[16][4];
+
+  rep(i, 16) {
+    char init = read_2x2_pattern();
+    read_4x4_pattern(evolutions[init]);
+  }
+
+  // first row is after 0 iterations, second after 1... DP
+  vector<int[16]> number_of_black_after_n_iterations;
+  number_of_black_after_n_iterations.reserve(16);
 
   return 0;
 }
