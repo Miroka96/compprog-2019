eba4fbf98ae70fd1eca85e333ed55de1fcbdb6e3
mirko.krause <mirko.krause@student.hpi.de>
2019-05-22 12:47:12 +0200

    solved automaton

--- 06/automaton/automaton.cpp
+++ 06/automaton/automaton.cpp
@@ -70,18 +70,44 @@ inline void print(const char *str) {
 
 ////////////////////////////// Task ////////////////////////
 
-inline char pattern_to_n(const bool& lu, const bool& ru, const bool& lb, const bool& rb) {
+constexpr static int mod = 998244353;
+
+typedef uint64_t mul_t;
+
+template <typename R = mul_t, typename T>
+inline T modadd(const T &a, const T &b, const T &modulus = mod) {
+  return (((R)a) + ((R)b)) % ((R)modulus);
+}
+
+template <typename R = mul_t, typename T>
+inline T modmul(const T &a, const T &b, const T &modulus = mod) {
+  return (((R)a) * ((R)b)) % ((R)modulus);
+}
+
+template <typename R = mul_t, typename T>
+inline R modpow(T base, T exp, const T &modulus = mod) {
+  base %= modulus;
+  R result = 1;
+  while (exp > 0) {
+    if (exp & 1)
+      result = modmul<R>(result, base, modulus);
+    base = modmul<R>(base, base, modulus);
+    exp >>= 1;
+  }
+  return result;
+}
+
+inline char pattern_to_n(const bool &lu, const bool &ru, const bool &lb,
+                         const bool &rb) {
   char res = 0;
   res += lu;
-  res += ru<<1;
-  res += lb<<2;
-  res += rb<<3;
+  res += ru << 1;
+  res += lb << 2;
+  res += rb << 3;
   return res;
 }
 
-inline bool read_field() {
-  return getchar_unlocked() == '#';
-}
+inline bool read_field() { return getchar_unlocked() == '#'; }
 
 inline char read_2x2_pattern() {
   bool lu, ru, lb, rb;
@@ -97,21 +123,29 @@ inline char read_2x2_pattern() {
 inline void read_4x4_pattern(char res[4]) {
   bool field[4][4];
   rep(i, 4) {
-    rep(j, 4) {
-      field[i][j] = read_field();
-    }
+    rep(j, 4) { field[i][j] = read_field(); }
     getchar_unlocked();
   }
 
   int k = 0;
-  for (int i = 0; i < 4; i+=2) {
-    for (int j = 0; j < 4; j+=2) {
-      res[k++] = pattern_to_n(
-        field[i][j],
-        field[i][j + 1],
-        field[i + 1][j],
-        field[i + 1][j + 1]
-      );
+  for (int i = 0; i < 4; i += 2) {
+    for (int j = 0; j < 4; j += 2) {
+      res[k++] = pattern_to_n(field[i][j], field[i][j + 1], field[i + 1][j],
+                              field[i + 1][j + 1]);
+    }
+  }
+}
+
+inline void modmul_layers(int posterior[16], int prior[16][16], int res[16]) {
+  rep(j, 16) {
+    int posterior_count = posterior[j];
+    if (posterior_count == 0)
+      continue;
+    rep(k, 16) {
+      int prior_count = prior[j][k];
+      if (prior_count == 0)
+        continue;
+      res[k] = modadd(res[k], modmul(prior_count, posterior_count));
     }
   }
 }
@@ -122,18 +156,46 @@ int main(int argc, char *argv[]) {
   uint64_t iteration_count;
   readn(iteration_count);
 
-  char initial = read_2x2_pattern();
+  char largest_power_of_2 = 64 - __builtin_clzll(iteration_count);
+  int pattern_counts[largest_power_of_2][16][16] = {};
 
-  char evolutions[16][4];
+  char initial = read_2x2_pattern();
 
   rep(i, 16) {
     char init = read_2x2_pattern();
-    read_4x4_pattern(evolutions[init]);
+    char results[4];
+    read_4x4_pattern(results);
+    rep(j, 4) { pattern_counts[0][init][results[j]] += 1; }
   }
 
-  // first row is after 0 iterations, second after 1... DP
-  vector<int[16]> number_of_black_after_n_iterations;
-  number_of_black_after_n_iterations.reserve(16);
+  for (int iter = 1; iter < largest_power_of_2; iter++) {
+    rep(i, 16) {
+      modmul_layers(pattern_counts[iter-1][i], pattern_counts[iter-1], pattern_counts[iter][i]);
+    }
+  }
+
+  int(&result_patterns)[16] = pattern_counts[largest_power_of_2 - 1][initial];
+
+  for (int iter = largest_power_of_2 - 2; iter >= 0; iter--) {
+    if ((iteration_count & (1L << iter)) == 0)
+      continue;
+
+    int iteration_results[16] = {};
+
+    modmul_layers(result_patterns, pattern_counts[iter], iteration_results);
+
+    rep(i, 16) {
+      result_patterns[i] = iteration_results[i];
+    }
+  }
+
+  int result = 0;
+
+  rep(i, 16) {
+    result = modadd(result, modmul(result_patterns[i], __builtin_popcount(i)));
+  }
+
+  write(result);
 
   return 0;
 }
