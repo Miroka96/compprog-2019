559c29fa0128a4aaeba96e3a73ccc28be4ead109
mirko.krause <mirko.krause@student.hpi.de>
2019-05-24 10:50:06 +0200

    extended template with Angelika's Input and tests

--- template/solution.cpp
+++ template/solution.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 
 ////////////////////////////// Helper //////////////////////
 
@@ -14,6 +15,89 @@
 #define all(a) (a).begin(), (a).end()
 
 using namespace std;
+
+////////////////////// Angelika's Input ////////////////////
+
+struct Input
+{
+	char *buf;
+
+	Input(size_t size) : buf((char *)malloc(size))
+	{
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cout.precision(10);
+
+		char *start = buf;
+		int n = 0;
+		do
+		{
+			buf += n;
+			n = read(STDIN_FILENO, buf, size);
+		} while (n > 0);
+		assert(buf <= start + size);
+		buf = start;
+	}
+
+	void skip_space()
+	{
+		while (*buf <= ' ')
+			buf++;
+	}
+
+	operator char()
+	{
+		skip_space();
+		return *buf++;
+	}
+
+	operator bool()
+	{
+		skip_space();
+		return *buf++ != '0';
+	}
+
+	operator char *()
+	{
+		skip_space();
+		char *s = buf;
+		while (*buf++ > ' ')
+			;
+		buf[-1] = '\0';
+		return s;
+	}
+
+	operator float()
+	{
+		return (double)*this;
+	}
+
+	operator double()
+	{
+		char *s = *this;
+		return atof(s);
+	}
+
+	template <typename T>
+	operator T()
+	{
+		skip_space();
+		T n = 0;
+		char c = *buf++;
+		bool neg = c == '-';
+		if (neg)
+			c = *buf++;
+		while (c > ' ')
+		{
+			n *= 10;
+			n += c - '0';
+			c = *buf++;
+		}
+		if (neg)
+			n = -n;
+		return n;
+	}
+};
 
 ////////////////////////////// I/O /////////////////////////
 #define BASE 10
