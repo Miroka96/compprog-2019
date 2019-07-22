e656d6e5bf9e3d5abafe9f310793bab4ac5f0614
mirko.krause <mirko.krause@student.hpi.de>
2019-07-22 18:12:34 +0200

    added fibonacci matrix implementation

---
+++ stuff/fibonacci/solution.cpp
@@ -0,0 +1,200 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+
+////////////////////////////// Helper //////////////////////
+
+// disable debugging for submission
+//#define debug(x)    x
+// print value of debug variable
+#define debug(x) clog << #x << " = " << x << endl
+
+// print a debug string
+#define debugs(str) // clog << str << endl
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define repd(a, b) for (int a = 0; a < (b); debug(++a))
+#define all(a) (a).begin(), (a).end()
+
+using namespace std;
+
+////////////////////// Angelika's Input ////////////////////
+
+struct Input {
+  char *buf;
+
+  Input(size_t size) : buf((char *)malloc(size)) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.precision(10);
+
+    char *start = buf;
+    int n = 0;
+    do {
+      buf += n;
+      n = read(STDIN_FILENO, buf, size);
+    } while (n > 0);
+    assert(buf <= start + size);
+    buf = start;
+  }
+
+  void skip_space() {
+    while (*buf <= ' ')
+      buf++;
+  }
+
+  operator char() {
+    skip_space();
+    return *buf++;
+  }
+
+  operator bool() {
+    skip_space();
+    return *buf++ != '0';
+  }
+
+  operator char *() {
+    skip_space();
+    char *s = buf;
+    while (*buf++ > ' ')
+      ;
+    buf[-1] = '\0';
+    return s;
+  }
+
+  operator float() { return (double)*this; }
+
+  operator double() {
+    char *s = *this;
+    return atof(s);
+  }
+
+  template <typename T> operator T() {
+    skip_space();
+    T n = 0;
+    char c = *buf++;
+    bool neg = c == '-';
+    if (neg)
+      c = *buf++;
+    while (c > ' ') {
+      n *= 10;
+      n += c - '0';
+      c = *buf++;
+    }
+    if (neg)
+      n = -n;
+    return n;
+  }
+};
+
+////////////////////////////// I/O /////////////////////////
+#define BASE 10
+#define OUTPUT_LENGTH 24
+#define BUFFER_SIZE 0x10000
+
+// buffer stdin
+char inputbuffer[BUFFER_SIZE];
+
+template <typename T> inline void readn(T &x) {
+  x = 0;
+  bool neg = 0;
+  register char c = getchar_unlocked();
+
+  if (c == '-')
+    neg = 1, c = getchar_unlocked();
+
+  for (; c >= '0' && c <= '9'; c = getchar_unlocked())
+    x = (x << 3) + (x << 1) + (c & 15);
+
+  if (neg)
+    x *= -1;
+}
+
+// create output buffer
+char outputbuffer[OUTPUT_LENGTH];
+
+template <typename T> inline void write(T n) {
+  bool neg = 0;
+  if (n < 0)
+    n *= -1, neg = 1;
+
+  int i = 0;
+  do {
+    outputbuffer[i++] = n % 10 + '0';
+    n /= 10;
+  } while (n);
+  --i;
+
+  if (neg)
+    putchar_unlocked('-');
+
+  while (i >= 0)
+    putchar_unlocked(outputbuffer[i--]);
+
+  putchar_unlocked('\n');
+}
+
+inline void print(const char *str) {
+  for (const char *c = str; *c; c++) {
+    putchar_unlocked(*c);
+  }
+}
+
+////////////////////////////// Task ////////////////////////
+
+struct matrix {
+  int height;
+  int width;
+  int** entries;
+
+  matrix(int h, int w) {
+    height = h;
+    width = w;
+    entries = new int*[height];
+    rep(i, height) {
+      entries[i] = new int[width];
+    }
+  }
+
+  matrix* mul(matrix &b) {
+    assert(width == b.height);
+    matrix* res = new matrix(height, b.width);
+
+    rep(w, b.width) {
+      rep(h, height) {
+
+        int sum = 0;
+
+        rep(i, b.height) {
+          sum += entries[h][i] * b.entries[i][w];
+        }
+
+        res->entries[h][w] = sum;
+      }
+    }
+
+    return res;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  setvbuf(stdin, inputbuffer, _IOFBF, BUFFER_SIZE);
+
+  matrix* fib = new matrix(2, 2);
+  fib->entries[0][0] = 1;
+  fib->entries[0][1] = 1;
+  fib->entries[1][0] = 1;
+  fib->entries[1][1] = 0;
+
+  matrix* num = new matrix(2,1);
+  num->entries[0][0] = 1;
+  num->entries[0][1] = 0;
+
+  write(0);
+  write(1);
+
+  while(true) {
+    num = fib->mul(*num);
+    write(num->entries[0][0]);
+  }
+
+  return 0;
+}
